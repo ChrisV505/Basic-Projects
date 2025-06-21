4fe461b4f2d6b615dfbaae1524dd7528a90320d2
christopher <christophervalle3338@gmail.com>
2025-06-20 22:55:28 -0700

    Added color to each type of button

--- calculator_javax/src/Calculator.java
+++ calculator_javax/src/Calculator.java
@@ -21,6 +21,16 @@ public class Calculator {
     JPanel displayPanel = new JPanel();
     JPanel buttonsPanel = new JPanel();
 
+    String[] buttonValues = {
+        "AC", "+/-", "%", "÷", 
+        "7", "8", "9", "×", 
+        "4", "5", "6", "-",
+        "1", "2", "3", "+",
+        "0", ".", "√", "="
+    };
+    String[] rightSymbols = {"÷", "×", "-", "+", "="};
+    String[] topSymbols = {"AC", "+/-", "%"};
+
     Calculator() {
         frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
@@ -30,7 +40,7 @@ public class Calculator {
         frame.setLayout(new BorderLayout());
 
         displayLabel.setBackground(customLavenderPurple);
-        displayLabel.setForeground(customCottonCandy);
+        displayLabel.setForeground(Color.white);
         displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
         displayLabel.setHorizontalAlignment(JLabel.RIGHT);
         displayLabel.setText("0");
@@ -40,6 +50,63 @@ public class Calculator {
         displayPanel.add(displayLabel);
         frame.add(displayPanel, BorderLayout.NORTH);
 
+        buttonsPanel.setLayout(new GridLayout(5, 4));
+        buttonsPanel.setBackground((customLavenderPurple));
+        frame.add(buttonsPanel);
+
+        for(int i = 0; i< buttonValues.length; i++) {
+            JButton button = new JButton();
+            String buttonValue = buttonValues[i];
+            button.setFont(new Font("Arial", Font.PLAIN, 30));
+            button.setText(buttonValue);
+            button.setFocusable(false);
+
+            if(Arrays.asList(topSymbols).contains(buttonValue)) {
+                button.setBackground(customPastelViolet);
+                button.setForeground(Color.white);
+            } 
+            else if(Arrays.asList(rightSymbols).contains(buttonValue)) {
+                button.setBackground(customThistle);
+                button.setForeground(Color.white);
+            } else {
+                button.setBackground(customCottonCandy);
+                button.setForeground(Color.white);
+            }
+            buttonsPanel.add(button);
+
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    JButton button = (JButton) e.getSource();
+                    String buttonValue = button.getText();
+                    if(Arrays.asList(rightSymbols).contains(buttonValue)) {
+
+                    }
+                    else if(Arrays.asList(topSymbols).contains(buttonValue)) {
+
+                    } else { //digits or .                        
+                        if(buttonValue == ".") {
+                            if(!displayLabel.getText().contains(buttonValue)) {
+                                displayLabel.setText(buttonValue);
+                            }
+                        } 
+                        else if("0123456789".contains(buttonValue)) {
+                            if(displayLabel.getText() == "0") {
+                                displayLabel.setText(buttonValue);
+                            } else {
+                                displayLabel.setText(displayLabel.getText().concat(buttonValue));
+                            }
+                        }
+                    }
+
+
+                }
+            });
+
+        }
+
+
+
+        
 
     }
 
