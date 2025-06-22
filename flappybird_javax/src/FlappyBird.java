e630c2e9ff92dfcbcecf502242c5956c7ff414a7
christopher <christophervalle3338@gmail.com>
2025-06-22 00:10:48 -0700

    Finished flappyBird and pipe logic/generation

--- flappybird_javax/src/FlappyBird.java
+++ flappybird_javax/src/FlappyBird.java
@@ -59,9 +59,12 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     int gravity = 1;
 
     ArrayList<Pipe> pipes;
+    Random random = new Random();
 
     Timer gameLoop;
     Timer placePipesTimer;
+
+    
 
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
@@ -87,6 +90,8 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
             }
         });
 
+        placePipesTimer.start();
+
         //game timer
         gameLoop = new Timer(1000/60, this); //1000/60 = 16.6;
         gameLoop.start();
@@ -94,8 +99,21 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     }
 
     public void placePipes() {
+        //(0 - 1) * pipeHeight/2 -> (0 - 256)
+        //128
+        //0 - 128 - (0-256) --> pipeHeight/4 -> 3/4 pipeHeight
+
+        int randomPipeY = (int) (pipeY - pipeHeight/4 - Math.random() * (pipeHeight/2));
+        int openingSpace = boardHeight/4;
+
+
         Pipe topPipe = new Pipe(topPipeImg);
+        topPipe.y = randomPipeY;
         pipes.add(topPipe);
+
+        Pipe bottomPipe = new Pipe(bottomPipeImg);
+        bottomPipe.y = topPipe.y + pipeHeight + openingSpace;
+        pipes.add(bottomPipe);
     }
 
     public void paintComponent(Graphics g) {
@@ -109,6 +127,13 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
 
         //bird
         g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+
+        //pipes
+        for(int i = 0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            g.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height, null);
+        }
+
     }
 
     public void move() {
@@ -116,6 +141,12 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
         velocityY += gravity;
         bird.y += velocityY;
         bird.y = Math.max(bird.y, 0);
+
+        //pipes
+        for(int i = 0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            pipe.x += velocityX;
+        }
     }
 
 
