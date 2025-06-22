9082318cb85424d3caf3b35e7e62ff6ff3dfad86
christopher <christophervalle3338@gmail.com>
2025-06-21 21:22:46 -0700

    Add main panel into frame

--- flappybird_javax/src/App.java
+++ flappybird_javax/src/App.java
@@ -7,10 +7,14 @@ public class App {
         int boardHeight = 640;
 
         JFrame frame = new JFrame("Flappy bird");
-        frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack(); //excludes the dimensions of the title bar
+        frame.setVisible(true);
     }
 }
