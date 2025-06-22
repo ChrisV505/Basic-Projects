f9d25dd8d6cbe0b77540e36777e43ca7975b3e11
christopher <christophervalle3338@gmail.com>
2025-06-21 21:16:43 -0700

    Add main JFrame & images for the game

---
+++ flappybird_javax/src/App.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+
+public class App {
+    public static void main(String[] args) {
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+        JFrame frame = new JFrame("Flappy bird");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
