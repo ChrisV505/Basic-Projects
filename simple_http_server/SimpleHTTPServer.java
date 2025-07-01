eaf16e06f261cdb9cedaaac9edca49c4c1d86ae7
christopher <christophervalle3338@gmail.com>
2025-06-30 22:07:18 -0700

    Finish simple server

--- simple_http_server/SimpleHTTPServer.java
+++ simple_http_server/SimpleHTTPServer.java
@@ -1,5 +1,25 @@
 package simple_http_server;
 
-public class SimpleHTTPServer {
-    
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class SimpleHTTPServer{
+    public static void main(String[] args) {
+    try {
+        System.out.println("Listening for connection on port 8080 ....");
+        while(true) {
+            try(ServerSocket server = new ServerSocket(8080); Socket socket = server.accept()) {
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+            }
+        }
+    } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+
+    }
 }
